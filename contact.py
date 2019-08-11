eadb07f48c4b4196847673dc5fa069d7cdbeb2a9
Flavio Mejia <flaviomejia@Flavios-Air.fritz.box>
2019-08-12 00:04:00 +0200

    A bug regarding the delete method has to be resolved

--- contact.py
+++ contact.py
@@ -1,9 +1,41 @@
 import sqlite3
 import click
+from dataclasses import dataclass
 
 # Click uses its own echo function. It's importants only if using ASCII colors
 
 connector = sqlite3.connect("contact_book.db")
+
+
+@dataclass
+class Contact:
+    """ The class is used to customize the string representation of the contact """
+
+    name: str
+    surname: str
+    phone: str
+    email: str
+    age: int
+
+    def __str__(self):
+        delim_rep = 20
+
+        fields = [
+            "",
+            "=" * delim_rep,
+            f"{self.name} {self.surname.upper()}",
+            self.phone,
+        ]
+
+        if self.email:
+            fields.append(self.email)
+
+        if self.age:
+            fields.append(f"Age: {self.age} years")
+
+        fields += ["=" * delim_rep, ""]
+
+        return "\n".join(fields)
 
 
 @click.group()
@@ -42,16 +74,23 @@ def reset():
 @click.option("--email", default=None, type=click.STRING)
 @click.option("--age", default=None, type=click.INT)
 def new(name, surname, phone, email, age):
+    """Create a new entry in the contact book, if it does not already exists"""
+
+    # Interrupt if entry exists
+    if contains(name, surname):
+        print(f"There is already an entry for {name} {surname}")
+        return
+
     insert = """
         INSERT INTO contacts
         VALUES (?, ?, ?, ?, ?)
         """
 
-    contact = (name, surname, phone, email, age)
+    contact = (name.capitalize(), surname.capitalize(), phone, email, age)
 
     connector.execute(insert, contact)
     connector.commit()
-    print(f"You added {contact} to your contacts")
+    print(f"You added: {Contact(*contact)} ")
 
 
 @cli.command()
@@ -72,24 +111,46 @@ def show(all):
         print(row)
         # print(f"{row[1]}, {row[0]}: {row[2]}")
 
+@cli.command()
+@click.argument("name", type=click.STRING)
+@click.argument("surname", type=click.STRING)
+def delete(name, surname):
+    name, surname = name.capitalize(), surname.capitalize()
+    cur = connector.cursor()
+    cur.execute("""
+        DELETE  FROM contacts
+        WHERE name=? AND surname=?""",
+        (name, surname)
+    )
 
-# setup_db(connector)
+#    result = connector.commit()
+
+    #print(f"{list(cur)}")
+
+    #print(list(query))
+
+    #if list(query) == []:
+        #print("There is no such entry")
+
+#    else: 
+ #       print(f"{name} {surname} was deleted.")
+
+def contains(name, surname):
+    """Check if the entry already exists in the contacts table"""
+    query = connector.execute(
+        """
+        SELECT * FROM contacts
+        WHERE name=? AND surname=?""",
+        (name.capitalize(), surname.capitalize()),
+    )
+
+    if list(query) == []:
+        return False
+
+    return True
+
+
 
 # add_contact(connector, "Karl", "Marx", "089123445", "karl.marx@posteo.de", 40)
 # add_contact(connector, "Groucho", "Marx", "8947529835", "groucho.marx@posteo.de")
 # add_contact(connector, "Friedrich", "Engels", "838575729")
-
-# show_all_contacts(connector)
-
-# connector.execute(
-#     """
-#     INSERT INTO contacts
-#     VALUES (?, ?, ?, ?, ?)
-#     """,
-#     karl_marx,
-# )
-# It is necessary to commit after each insert!
-# connector.commit()
-
-if __name__ == "main":
-    reset()
--- features.md
+++ features.md
@@ -1,5 +1,7 @@
-Future Features:
+# Future Features
 
+* Create a script to initialize the data
+* Check if the contact already in the contact book
 * Select which columns to show
 * Create an option to select the clumn by which results are sorted
 * Create edit() command to change and update the contacts
